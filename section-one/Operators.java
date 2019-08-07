24f10b7318411f09938ea376a77fd004ce5507de
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-08-07 15:32:27 -0400

    completes Operators section of java course

--- section-one/Operators.java
+++ section-one/Operators.java
@@ -1,3 +1,5 @@
+// import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
 public class Main {
     
     public static void main(String[] args) {
@@ -35,7 +37,41 @@ public class Main {
         result -=2; // 10 -2 = 8
         System.out.println("10 - 2 = " + result);
 
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien");
+            System.out.println("And I am scared of aliens");
+        }
 
+        int topScore = 80; 
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+        // Single & is a bitwise operate, same with single |
+        int secondTopScore = 81; 
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than the second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("If either or both of conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+        // ! logical complement operator 
+        boolean isCar = true;
+        if(!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        // Ternary operator shortcut of if else statement
+        boolean wasCar = isCar ? true: false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        } 
+        
     }
-
 }
