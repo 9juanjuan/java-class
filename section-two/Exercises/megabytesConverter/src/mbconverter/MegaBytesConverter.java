444f20367b4d718e2040e8bf96ba9b1e36e4e7be
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-08-18 14:18:46 -0400

    adds java project to solve MegaBytes coding exercise

---
+++ section-two/Exercises/megabytesConverter/src/mbconverter/MegaBytesConverter.java
@@ -0,0 +1,16 @@
+package mbconverter;
+
+public class MegaBytesConverter {
+	public static void main(String[] args) {
+		printMegaBytesAndKiloBytes(2200);
+	}
+	public static void printMegaBytesAndKiloBytes (int kiloBytes) {
+		if (kiloBytes < 0) {
+			System.out.println("Invalid Value");
+	    } else {
+	        int mb = kiloBytes / 1024;
+	        int kb =  kiloBytes -(mb * 1024);
+	        System.out.println(kiloBytes + " KB = " + mb + " MB and " + kb + " KB");
+	     }
+	 }
+}
