7e4f570dbe19ba17cf24d4ed4ce546a7b3c1df71
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-08-19 15:36:51 -0400

    adds Person and PersonLinkedList class to handle string objects with linked lists

--- data-structures/LinkedList/src/linkedList/Main.java
+++ data-structures/LinkedList/src/linkedList/Main.java
@@ -3,15 +3,24 @@ package linkedList;
 public class Main {
 
 	public static void main(String[] args) {
-		IntLinkedList list = new IntLinkedList();
-		list.insertItem(5);
-		list.insertItem(2);
-		list.insertItem(8);
+//		IntLinkedList list = new IntLinkedList();
+//		list.insertItem(5);
+//		list.insertItem(2);
+//		list.insertItem(8);
+//		list.printList();
+//		
+//		list.sortList();
+//		System.out.println();
+//		list.printList(); 
+//		
+		Person person1 = new Person ("Juan", "123");
+		Person person2 = new Person ("Antonio", "321");
+		Person person3 = new Person ("Garcia", "122");
+
+		PersonLinkedList list = new PersonLinkedList(person1);
+		list.insertItem(person2); 
+		list.insertItem(person3); 
+		
 		list.printList();
-		
-		list.sortList();
-		System.out.println();
-		list.printList(); 
-		
 	}
 }
